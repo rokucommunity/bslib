4b52f9d1afd66cc7b4db399b7a84b2de38428d70
Bronley <bronley@gmail.com>
2021-02-23 22:46:41 -0500

    Add script to export bslib content

---
+++ index.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var path = require('path');
+
+module.exports = {
+    version: JSON.parse(
+        fs.readFileSync(
+            path.join(__dirname, 'package.json')
+        ).toString()
+    ).version,
+    //load the contents of bslib.brs
+    source: fs.readFileSync(path.join(__dirname, 'source', 'bslib.brs')).toString()
+};
+console.log(module.exports);
